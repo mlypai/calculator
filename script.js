c4f23654ac20d688c56aa69a4ead98df68777dff
mlypai <balladbell@gmail.com>
2023-02-28 19:39:00 +0200

    linking operations

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".special");
 
 let display = document.getElementById('display');
-let displayValue;
+let displayValue = 0;
+let operator;
 
 
+function pow(x, y)
+{
+    return Math.pow(x, y);
+}
 
 function add(x, y)
 {
@@ -36,6 +42,8 @@ function operate(x, op, y)
             return mul(x, y);
         case '/':
             return divide(x, y);
+        case '^':
+            return pow(x, y);
         default:
             return;
     }
@@ -43,8 +51,50 @@ function operate(x, op, y)
 
 function popDisplay()
 {
-    if(display.textContent.length < 9)
+    if(display.textContent === '0')
+        display.textContent = this.textContent;
+    else if(display.textContent.length < 9)
         display.textContent += this.textContent;
 }
 
+function operations()
+{
+    switch(this.textContent){
+        case "+/-":
+            display.textContent *= -1;
+            break;
+        case "AC":
+            display.textContent = 0;
+            displayValue = 0;
+            operator = 0;
+            break;
+        case "C":
+            display.textContent = display.textContent
+                                    .split('')
+                                    .splice(0, display.textContent.length -1)
+                                    .join('');
+            break;
+        case ".":
+            if (!display.textContent.split('').find(a => a === "."))
+                display.textContent += '.';
+            break;
+        default:
+            if(!operator)
+            {
+                displayValue = display.textContent;
+                operator = this.textContent;
+                display.textContent = "";
+            }
+            else
+            {
+                display.textContent = operate(Number(displayValue), operator, Number(display.textContent));
+                displayValue = 0;
+                operator = '';
+            }    
+            break;
+    }
+}
+
 numbers.forEach(a => a.addEventListener('click', popDisplay));
+operators.forEach(a => a.addEventListener('click', operations))
+
