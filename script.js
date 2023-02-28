acd93e1e72b3cc234c784dc33aa7c577a7890217
mlypai <balladbell@gmail.com>
2023-02-28 20:59:34 +0200

    try not to overflow

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ function operations()
                 displayValue = operate(Number(displayValue), operator, Number(display.textContent));
                 if(displayValue >= 1e+9)
                     displayValue = displayValue.toExponential(1);
-                display.textContent = displayValue;
+                display.textContent = displayValue.toFixed(2);
                 if(this.textContent != '=')
                     operator = this.textContent;
                 else
